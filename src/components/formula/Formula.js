ad23c800b7e10f6c16cd56d3d58ed2abf6221ae6
Alex Dang <dang@nixsolutions.com>
2020-08-18 16:48:48 +0300

    Excel SPA

---
+++ src/components/formula/Formula.js
@@ -0,0 +1,46 @@
+import {ExcelComponent} from '@core/ExcelComponent';
+import {$} from '@core/dom';
+
+export class Formula extends ExcelComponent {
+  static className = 'excel__formula';
+
+  constructor($root, options) {
+    super($root, {
+      name: 'Formula',
+      listeners: ['input', 'keydown'],
+      subsribe: ['currentText'],
+      ...options
+    });
+  }
+
+  toHTML() {
+    return `
+      <div class="info">fx</div>
+      <div class="input" id="formula" contenteditable spellcheck="false"></div>
+    `;
+  }
+
+  init() {
+    super.init();
+    this.$formula = this.$root.find('#formula');
+    this.$on('table:select', $cell => {
+      this.$formula.text($cell.data.value);
+    });
+  }
+
+  onInput(event) {
+    this.$emit('formula:input', $(event.target).text());
+  }
+
+  storeChanged({currentText}) {
+    this.$formula.text(currentText);
+  }
+
+  onKeydown(event) {
+    const keys = ['Entet', 'Tab'];
+    if (keys.includes(event.key)) {
+      event.preventDefault();
+      this.$emit('formula:done');
+    }
+  }
+}
